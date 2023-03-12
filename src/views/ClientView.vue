1175896b1938c358a8a52c74ed3e562e26d386b3
Mars <zm03322161@qq.com>
2023-03-12 21:48:57 +0800

    update

--- src/views/ClientView.vue
+++ src/views/ClientView.vue
@@ -1,64 +1,114 @@
 <template>
-    <div class="connect" v-if="isShowConnect">
-      <el-input v-model="inputValue" placeholder="接入码" class="cinput"/>
-      <el-button round class="cbtn" @click="toConnect">接入</el-button>
-    </div>
-    <el-button round class="cbtn" @click="checkConnect">查询状态</el-button>
+  <div class="common-layout">
+    <el-row>
+      <el-container>
+        <div class="connect" v-if="isShowConnect">
+            <el-input v-model="inputValue" placeholder="接入码" class="cinput" />
+            <el-button round class="cbtn" @click="toConnect">接入</el-button>
+          </div>
+      </el-container>
+    </el-row>
+    <!-- <el-row >
+      <el-col :span="24" >
+        <el-button round class="cbtn" @click="checkConnect">查询状态</el-button>
+      </el-col>
+    </el-row> -->
+
+    <el-row class="bees">
+      <el-col :span="6">
+        <el-card class="box-card">
+          <template #header>
+          <div class="card-header">
+            <span>名称</span>
+          </div>
+        </template>
+          <el-button round>校验</el-button>
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card class="box-card"></el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card class="box-card"></el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card class="box-card"></el-card>
+      </el-col>
+    </el-row>
+    
+  </div>
 </template>
 
 <script lang="ts">
-import { ElMessage } from 'element-plus'
-export default {
-  data() {
-    return {
-      inputValue:"",
-      isShowConnect:true
-    }
-  },
-  methods:{
-    toConnect(){
-      console.log("connect")
-      this.axios.post("/cvConnect",{'key':this.inputValue}
-      ).then(res=>{
-        console.log(res.data);
-        this.showMssage(res.data)
-        
-      })
+  import {
+    ElMessage
+  } from 'element-plus'
+  export default {
+    data() {
+      return {
+        inputValue: "",
+        isShowConnect: true
+      }
     },
-    checkConnect(){
-      this.axios.get("/checkConnect").then(res=>{
-        console.log(res.data);
-      })
-    },
-    showMssage(resposne){
-      var code = resposne.code
-      if(code=='200'){
+    methods: {
+      toConnect() {
+        console.log("connect")
+        this.axios.post("/cvConnect", {
+          'key': this.inputValue
+        }).then(res => {
+          console.log(res.data);
+          this.showMssage(res.data)
+
+        })
+      },
+      checkConnect() {
+        this.axios.get("/checkConnect").then(res => {
+          console.log(res.data);
+        })
+      },
+      showMssage(resposne) {
+        var code = resposne.code
+        if (code == '200') {
           this.showSuccess("链接正常")
-          this.isShowConnect=false
+          this.isShowConnect = false
         }
-        if(code=='400'){
+        if (code == '400') {
           this.showError(resposne.message)
         }
-    },
-    showSuccess(msg){
-      ElMessage({
-        message:msg,
-        type:"success"
-      })
-    },
-    showError(msg){
-      ElMessage.error(msg)
+      },
+      showSuccess(msg) {
+        ElMessage({
+          message: msg,
+          type: "success"
+        })
+      },
+      showError(msg) {
+        ElMessage.error(msg)
+      }
     }
   }
-}
 </script>
 
 <style lang="less" scoped>
-.connect{
-  display: flex;
-  flex-direction: column;
-  .cbtn{
+  .bees{
+    width: 100%;
+  }
+  .box-card{
     margin: 10px;
   }
-}
+  .common-layout{
+    width: 100%;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    display: flex;
+  }
+  .connect {
+    display: flex;
+    flex-direction: column;
+    .cbtn {
+      margin: 10px;
+    }
+  }
 </style>
